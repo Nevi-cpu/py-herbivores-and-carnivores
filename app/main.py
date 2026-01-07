181651012c1424862afccae6940b68f26996fd5e
Andrii Usypiv <anriyusypiv@gmail.com>
2026-01-07 13:07:37 +0200

    Result

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+from typing import List, Any
+
+
+class Animal:
+    alive: List[Any] = []
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self._health = 100
+        self.hidden = False
+        Animal.alive.append(self)
+
+    @property
+    def health(self) -> int:
+        return self._health
+
+    @health.setter
+    def health(self, value: int) -> None:
+        self._health = value
+        if self in Animal.alive and self._health <= 0:
+            Animal.alive.remove(self)
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, target: Animal) -> None:
+        if isinstance(target, Herbivore) and not target.hidden:
+            target.health -= 50
